a571c91ac79381bae59314746a2ae8de734e1e35
Luiza <khaj-luizkin@yandex.ru>
2022-08-01 21:15:13 +0300

    верстка входного экрана

---
+++ MakeSomeFood/LoginViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginWithAppleAccountButton: UIButton!
+    @IBOutlet weak var loginWithGoogleAccountButton: UIButton!
+    @IBOutlet weak var scrollViewBottom: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            scrollViewBottom.constant = keyboardSize.height
+        }
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        scrollViewBottom.constant = 0
+    }
+
+}
+
