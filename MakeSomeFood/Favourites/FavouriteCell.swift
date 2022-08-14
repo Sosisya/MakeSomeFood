94ecf583642ed3ca79a87942db4ca11e45cc335f
Luiza <khaj-luizkin@yandex.ru>
2022-08-14 17:57:59 +0300

    favoutieView

--- MakeSomeFood/Favourites/FavouriteCell.swift
+++ MakeSomeFood/Favourites/FavouriteCell.swift
@@ -2,11 +2,35 @@ import UIKit
 
 class FavouriteCell: UITableViewCell {
 
+    @IBOutlet weak var faroutiteCategoryView: UIView!
+    @IBOutlet weak var faroutiteCategoryImage: UIImageView!
+    @IBOutlet weak var descriptionOfSelectedRecepie: UILabel!
+    @IBOutlet weak var areaTag: UILabel!
+    @IBOutlet weak var categoryTag: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        setTags(categoryTag)
+        setTags(areaTag)
+        setView(faroutiteCategoryView)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+
+    private func setTags(_ label: UILabel) {
+        label.layer.masksToBounds = true
+        label.layer.backgroundColor = UIColor.white.cgColor
+        label.layer.borderColor = label.textColor.cgColor
+        label.layer.borderWidth = 1
+        label.layer.cornerRadius = label.bounds.height / 2
+    }
+
+    private func setView(_ view: UIView) {
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 12
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.systemGray2.cgColor
     }
 }
