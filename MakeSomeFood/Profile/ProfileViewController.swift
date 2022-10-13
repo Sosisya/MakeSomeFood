ed5e08189649c911cd0caf16cc11813ea1d71dc4
Luiza <khaj-luizkin@yandex.ru>
2022-10-13 08:13:38 +0300

    Alert

--- MakeSomeFood/Profile/ProfileViewController.swift
+++ MakeSomeFood/Profile/ProfileViewController.swift
@@ -10,7 +10,7 @@ class ProfileViewController: UIViewController, UITextViewDelegate, UITextFieldDe
     }
 
     @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var takePhotoImage: UIImageView!
+    @IBOutlet weak var takePhotoButton: UIButton!
     @IBOutlet weak var exitButton: UIButton!
 
     @IBOutlet weak var nameTextField: FloatingTitleTextField!
@@ -37,7 +37,7 @@ class ProfileViewController: UIViewController, UITextViewDelegate, UITextFieldDe
         self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont(name: "Montserrat-SemiBold", size: 24)!]
 
         profileImage.layer.cornerRadius = 12
-        takePhotoImage.layer.cornerRadius = 22
+        takePhotoButton.layer.cornerRadius = 22
 
         nameTextField.textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         emailTextField.textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
@@ -68,5 +68,53 @@ class ProfileViewController: UIViewController, UITextViewDelegate, UITextFieldDe
 
     private func reloadSaveButton() {
         saveButton.isHidden = !hasChanges
+    }
+
+    @IBAction func showPhotoAllert(_ sender: UIButton) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+        alert.addAction(UIAlertAction(title: "Сделать фото", style: .default , handler:{ (UIAlertAction)in
+            print("User click Approve button")
+        }))
+
+        alert.addAction(UIAlertAction(title: "Выбрать из галлереи", style: .default , handler:{ (UIAlertAction)in
+            print("User click Edit button")
+        }))
+
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler:{ (UIAlertAction)in
+            print("User click Dismiss button")
+        }))
+
+        self.present(alert, animated: true, completion: {
+            print("completion block")
+        })
+    }
+
+    @IBAction func showExitAllert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Вы уверены что хотите выйти из профиля?", message: nil, preferredStyle: .actionSheet)
+
+            alert.addAction(UIAlertAction(title: "Выйти из профиля", style: .default , handler:{ (UIAlertAction)in
+                print("User click Approve button")
+            }))
+
+//            alert.addAction(UIAlertAction(title: "Edit", style: .default , handler:{ (UIAlertAction)in
+//                print("User click Edit button")
+//            }))
+//
+//            alert.addAction(UIAlertAction(title: "Delete", style: .destructive , handler:{ (UIAlertAction)in
+//                print("User click Delete button")
+//            }))
+
+            alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler:{ (UIAlertAction)in
+                print("User click Dismiss button")
+            }))
+
+
+            //uncomment for iPad Support
+            //alert.popoverPresentationController?.sourceView = self.view
+
+            self.present(alert, animated: true, completion: {
+                print("completion block")
+            })
     }
 }
